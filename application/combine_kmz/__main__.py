dbf4947d0fa0e99210fd7e74e016848e19ffa227
Ramadhan <ramiqcom@gmail.com>
2025-07-01 23:27:00 +0200

    fix fgb direct

--- application/combine_kmz/__main__.py
+++ application/combine_kmz/__main__.py
@@ -4,16 +4,28 @@ from tempfile import TemporaryDirectory
 temp_folder = TemporaryDirectory(delete=False)
 
 data_prefix = "data/KMZ/*.kmz"
-output = "output/seagrass_plot"
+output = "output/seagrass_plot.fgb"
 
+tmp_merge = f"{temp_folder.name}/merged.fgb"
 check_call(
     f"""ogrmerge \
       -single \
       -nln seagrass_plot \
-      -f "ESRI Shapefile" \
-      -lco SPATIAL_INDEX=YES \
-      -o {output} \
+      -f "FlatGeobuf" \
+      -o {tmp_merge} \
       {data_prefix}
+  """,
+    shell=True,
+)
+
+check_call(
+    f"""ogr2ogr \
+      -makevalid \
+      -nln seagrass_plot \
+      -f "FlatGeobuf" \
+      -lco SPATIAL_INDEX=YES \
+      {output} \
+      {tmp_merge}
   """,
     shell=True,
 )
